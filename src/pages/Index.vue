9d3de108e49756227ca3d1f1aac771fb83d6f979
patryckx <patrick.monteiro@w3as.com>
2019-02-13 17:16:55 -0300

    first commit :tada:

---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    {{ apiAtual }}
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      apiAtual: process.env.API
+    }
+  }
+}
+</script>
